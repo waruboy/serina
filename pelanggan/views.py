dba2817e66d5ddec5e3fea6ecb5fa343bae8ae23
Taufiq Muhammadi <waruboy@yahoo.com>
2013-05-22 19:00:19 +0700

    daftar pinjaman di detail pelanggan

--- pelanggan/views.py
+++ pelanggan/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from keranjang.keranjang import set_keranjang
 from toko.decorators import cek_izin
 from toko.utils import inisiasi_view
+from peminjaman.models import ItemPeminjaman, Peminjaman
 from .forms import UbahPelangganForm
 from .models import Pelanggan 
 
@@ -12,12 +13,18 @@ def depan(request, kode_toko=''):
 	(pengguna, toko) = inisiasi_view(request, kode_toko)
 	pelanggan = Pelanggan.objects.filter(toko=toko)
 	return render(request, 'pelanggan/depan.jade', locals())
+
 @login_required
 @cek_izin
 def detail(request, kode_toko='', kode_pelanggan=''):
 	(pengguna, toko) = inisiasi_view(request, kode_toko)
 	pelanggan = Pelanggan.objects.get(toko=toko, 
 		slug=kode_pelanggan)
+	peminjaman = Peminjaman.objects.filter(pelanggan=pelanggan)
+	catatan_peminjaman = []
+	for p in peminjaman:
+		item_peminjaman = ItemPeminjaman.objects.filter(peminjaman=p)
+		catatan_peminjaman.append(item_peminjaman)
 	if request.method == "POST":
 		set_keranjang(request, pelanggan)
 	return render(request, 'pelanggan/detail.jade', locals())
