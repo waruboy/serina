ecba864f6df893d1b447fb507a94eee909f7f210
Taufiq Muhammadi <waruboy@yahoo.com>
2013-07-04 06:10:09 +0700

    perbaiki fungsi lihat pesanan dan templatetag pesanan

--- pesanan/templatetags/pesanan_tags.py
+++ pesanan/templatetags/pesanan_tags.py
@@ -1,9 +1,10 @@
 from django import template
-from pesanan import utils
+from pesanan.utils import cek_pesanan
 
 register = template.Library()
 
 @register.inclusion_tag("tags/link_pesanan.jade")
 def link_pesanan(request, toko):
+	ada_pesanan = cek_pesanan(request)
 	kode_toko = toko.slug
 	return locals() 
