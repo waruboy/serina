cddba92da325c0ef6ab7772439696f1ca75dc0b4
Taufiq Muhammadi <waruboy@yahoo.com>
2013-07-09 05:36:44 +0700

    pencarian item

--- cari/mesin_cari.py
+++ cari/mesin_cari.py
@@ -1,6 +1,8 @@
+from django.db.models import Q
+from katalog.models import Item
 from pelanggan.models import Pelanggan
 from .models import Pencarian
-from django.db.models import Q
+
 
 KATA_DIHAPUS = []
 
@@ -16,13 +18,25 @@ def simpan(request, kata):
 # ambil pelanggan yang sesuai dengan pencarian
 def pelanggan(toko, kata_pencarian):
 	kata = _siapkan_kata(kata_pencarian)
-	pelanggan = Pelanggan.objects.filter(toko=toko)
+	pelanggan = Pelanggan.objects.filter(toko=toko, diaktifkan=True)
 	hasil = {}
 	hasil['pelanggan'] = []
 	for k in kata:
 		pelanggan = pelanggan.filter(Q(telepon__iexact=k))
 		hasil['pelanggan'] = pelanggan
-	return hasil
+	return hasil['pelanggan']
+
+def item(toko, kata_pencarian):
+	kata = _siapkan_kata(kata_pencarian)
+	item = Item.objects.filter(
+		aktif=True)
+	hasil = {}
+	hasil['item'] = []
+	for k in kata:
+		item = item.filter(Q(nama__exact=k))
+		hasil['item'] = item
+	return hasil['item']
+
 
 # hapus kata umum, limit sampai 5 kata
 def _siapkan_kata(kata_pencarian):
--- cari/views.py
+++ cari/views.py
@@ -5,5 +5,13 @@ from toko.utils import inisiasi_view
 def hasil(request, kode_toko):
 	(pengguna, toko) = inisiasi_view(request, kode_toko)
 	q = request.GET.get('q', '')
-	cocok = mesin_cari.pelanggan(toko, q)
+	permintaan = request.GET.get('submit')
+	if permintaan == "cari_item":
+		cocok = mesin_cari.item(toko,q)
+		objek_cari = "Item"
+		kolom_cari = "nama"
+	else:
+		cocok = mesin_cari.pelanggan(toko, q)
+		objek_cari = "Pelanggan"
+		kolom_cari = "telepon" 
 	return render(request, 'cari/hasil_cari.jade', locals())
